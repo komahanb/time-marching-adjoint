1090379e95a289abc1afe6c5368f1fdac28f11bf
komahan <komahan.cool@gmail.com>
2016-12-16 13:00:08 -0500

    modified matrix parent class with complex/real type definitions

--- src/matrix_class.f90
+++ src/matrix_class.f90
@@ -1,108 +1,115 @@
+#include "scalar.fpp"
+
+!=====================================================================!
+! Module that contains matrix type definition and procedures to add
+! and get entries from the matrix. This class also defines the
+! interface to add elements into the matrix.
+!
+! Author: Komahan Boopathy (komahan@gatech.edu)
+!=====================================================================!
+
 module matrix_class
 
-  ! module options
   implicit none
+
   private
   public :: matrix
 
-  ! module definitions
   type, abstract :: matrix
+     
+     ! type options
+     private
 
-    ! type options
-    private
+     ! attributes
+     integer :: row_size ! the number of rows in a matrix
+     integer :: col_size ! the number of columns in a matrix
 
-    ! attributes
-    integer :: row_size ! the number of rows in a matrix
-    integer :: col_size ! the number of columns in a matrix
+   contains
 
-    ! procedures
-    contains
-
-      ! included below
-      procedure :: get_col_size
-      procedure :: get_row_size
-      procedure :: set_col_size
-      procedure :: set_row_size
-
-      ! deferred until later
-      procedure(add_element_interface), deferred :: add_element
-
+     ! defined procedures
+     procedure :: get_col_size
+     procedure :: get_row_size
+     procedure :: set_col_size
+     procedure :: set_row_size
+     
+     ! deferred procedures
+     procedure(add_element_interface), deferred :: add_element
+     
   end type matrix
 
-  ! list of abstract interfaces
+  ! define interfaces to all abstract procedures
+ 
   abstract interface
+     
+     !================================================================!
+     ! interface to adding elements into the matrix
+     !================================================================!
+     
+     subroutine add_element_interface(this, row, col, val)
 
-    subroutine add_element_interface(this,row,col,val)
-      import matrix
-      class(matrix) :: this
-      integer :: row
-      integer :: col
-      real(8) :: val
-    end subroutine add_element_interface
+       import matrix
+
+       class(matrix) :: this
+       type(integer) :: row
+       type(integer) :: col
+       type(scalar)  :: val
+
+     end subroutine add_element_interface
 
   end interface
 
-  ! procedures
-  contains
-
-!===============================================================================
-! returns the column size
-!===============================================================================
+contains
+  
+  !===================================================================!
+  ! returns the column size
+  !===================================================================!
 
   function get_col_size(this)
 
-    ! arguments
-    integer :: get_col_size
     class(matrix) :: this
-
-    ! begin execution
+    type(integer) :: get_col_size
+    
     get_col_size = this % col_size
 
   end function get_col_size
 
-!===============================================================================
-! returns the row size
-!===============================================================================
+  !===================================================================!
+  ! returns the row size
+  !===================================================================!
 
   function get_row_size(this)
 
-    ! arguments
-    integer :: get_row_size
     class(matrix) :: this
+    type(integer) :: get_row_size
 
-    ! begin execution
     get_row_size = this % row_size
 
   end function get_row_size
 
-!===============================================================================
-! sets the row size
-!===============================================================================
+  !===================================================================!
+  ! sets the row size
+  !===================================================================!
 
   subroutine set_row_size(this, row)
 
-    ! arguments
     class(matrix) :: this
-    integer :: row
+    type(integer) :: row
 
-    ! begin execution
     this % row_size = row
-
+    
   end subroutine set_row_size
-
-!===============================================================================
-! sets the column size
-!===============================================================================
-
-  subroutine set_col_size(this, col)
-
-    ! arguments
-    class(matrix) :: this
-    integer :: col
-
-    ! begin execution
-    this % col_size = col
   
-  end subroutine set_col_size
+  !===================================================================!
+  ! sets the column size
+  !===================================================================!
+  
+  subroutine set_col_size(this, col)
+    
+    class(matrix) :: this
+    type(integer) :: col
 
+    this % col_size = col
+
+  end subroutine set_col_size
+  
 end module matrix_class
